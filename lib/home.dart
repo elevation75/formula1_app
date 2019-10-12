f5ae003b611bb5b905f99df3d6e94dd7e0f2b248
Elevation75 <just.elevation@gmail.com>
2019-10-12 16:21:48 +0300

    improved code

--- lib/home.dart
+++ lib/home.dart
@@ -4,11 +4,26 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Text(
-        "F1 webpage",
-        style: TextStyle(fontSize: 25.0),
-      )),
+      appBar: AppBar(
+       title: Text('Formula1 home'),
+     ),
+     bottomNavigationBar: BottomNavigationBar(
+       currentIndex: 0, // this will be set when a new tab is tapped
+       items: [
+         BottomNavigationBarItem(
+           icon: new Icon(Icons.info),
+           title: new Text('Teams'),
+         ),
+         BottomNavigationBarItem(
+           icon: new Icon(Icons.person_pin),
+           title: new Text('Drivers'),
+         ),
+         BottomNavigationBarItem(
+           icon: Icon(Icons.map),
+           title: Text('Races'),
+         )
+       ],
+     ),
     );
   }
 }
